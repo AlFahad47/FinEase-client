866b4beb74a4afdba40b27337c011b3d7edecb85
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-11-09 12:43:42 +0600

    responsive navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,96 @@
+import { NavLink } from "react-router";
+import logoImg from "../assets/logo.png";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/add-transaction">Add Transaction</NavLink>
+      </li>
+      <li>
+        <NavLink to="/my-transactions">My Transaction</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/reports">Reports </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <NavLink
+            to="/"
+            className="flex justify-center items-center font-bold "
+          >
+            <img className="h-10 w-10 mr-2 " src={logoImg} alt="" />
+            FinEase
+          </NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+        </div>
+        {/* profile */}
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>fahad@gmail.com</a>
+              </li>
+              <li>
+                <a>Fahad Molla</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
